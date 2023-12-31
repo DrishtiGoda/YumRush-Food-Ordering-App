8ce91ee8b94587d6d51a7e795dc4c2c132d3aec3
DrishtiGoda <goda.d@northeastern.edu>
2023-12-31 02:01:29 -0500

    Ep-01-Inception

---
+++ app.js
@@ -0,0 +1,20 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h1", {}, "I am h1 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h1", {}, "I am h1 tag"),
+  ]),
+]);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
+
+
+// above code is very messy
+// To solve this problem , we have => JSX 
